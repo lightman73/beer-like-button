022331e531bfab518eee6c84da88203fc5f5f86a
Francesco Marini <franz.marini@gmail.com>
2022-05-24 19:00:31 +0200

    Initial Commit

---
+++ beer-like-button/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beer-like-button
+//
+//  Created by Francesco Marini on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beer-like-button/beer_like_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  beer_like_buttonApp.swift
+//  beer-like-button
+//
+//  Created by Francesco Marini on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct beer_like_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
